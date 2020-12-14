e6c04228f409768dcd7a7e750ab1812460c31822
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-14 16:19:41 +0100

    #15 utworzenie pliku Crosswords.java

---
+++ Crosswords.java
@@ -0,0 +1,18 @@
+/*
+Stwórz krzyżówkę. które hasłami będą: wilk, kot, żyrafa (typy proste). 
+'----
+wilk
+kot
+żyrafa
+'----
+1) wyświetl trzecią literę drugiego hasła, 
+2) wyświetl pierwszą literę pierwszego hasła, 
+3) zamień ostatnią literę trzeciego hasła na 'y', 
+4) wyświetl długość drugiego hasła krzyżówki, 
+5) wyświetl całą krzyżówkę.  */
+
+class Crosswords(String args []) {
+    public static void main {
+        
+    }
+}
