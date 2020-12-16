eeb68ed310dd569faf41ae70c321e1a66c611c3c
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-16 11:38:07 +0100

    ponowny commit

--- Oceny2.java
+++ Oceny2.java
@@ -18,28 +18,19 @@ class Oceny2{
         System.out.print(" "+"\n");
         
         //popraw druga ocenę na 5
-        byte temp5 = 5;
+        grades[1] = 5;
                 
-        System.out.println("Druga ocena poprawiona na 5: ");
-        System.out.print(grades[2]+", ");
-        System.out.print(temp5+", " );
-        System.out.println(grades[4]);
-        System.out.print(" "+"\n");
-               
-        byte tmp0 = grades[1];
-        byte tmp1 = grades[4]; 
-        byte tmp2 = grades[2]; 
-        byte tmp3 = grades[0];
-        byte tmp4 = grades[3];   
-               
-        System.out.println("Oceny od najmniejszej do największej: ");
-        System.out.print(tmp0+", ");
-        System.out.print(tmp1+", ");  
-        System.out.print(tmp2+", ");
-        System.out.print(tmp3+", ");
-        System.out.println(tmp4);
-        System.out.print(" "+"\n");
        
+        // posortuj od najmniejszejdo największej: 
+        byte temp;
+        temp = grades[0]; 
+        grades[0] = grades [4];
+        grades [4] = temp; 
+                      
+        System.out.println("Oceny od najmniejszej do największej: ");
+        System.out.print(grades[0]+", "+grades[1]+", "+grades[2]+", "+grades[3]+", "grades[4]);
+        System.out.print();
+       //" "+"\n"
         
         
         
